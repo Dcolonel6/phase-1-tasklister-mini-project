85e0ac48cc3572e7dfcaf83211e1d4eea91d694e
frustrated-coder <d.n_mwangi@outlook.com>
2022-08-23 19:50:50 +0300

    finished assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // your code here  
+  const form = document.getElementById('create-task-form') 
+  form.addEventListener('submit', formSubmitHandler)
 });
+
+function formSubmitHandler(e) {
+  e.preventDefault()
+  const { target } = e
+  const value = target['new-task-description'].value
+  const newLi = createDomEle('li',titleCase(value))
+  appendElement(newLi)
+  target.reset()
+}
+
+function createDomEle(eleToCreate,newTodo){
+  const element = document.createElement(eleToCreate)
+  element.textContent = newTodo
+  return element 
+
+}
+function appendElement(element, elemntToAppendTo=document.getElementById('tasks')){
+  elemntToAppendTo.append(element)  
+}
+
+function titleCase(sentence){
+  const words = sentence.split(' ')
+  return words.map(word => {
+    const lowerCased = word.toLowerCase()
+    return `${lowerCased[0].toUpperCase()}${lowerCased.slice(1)}`
+  }).join(" ")
+
+}
